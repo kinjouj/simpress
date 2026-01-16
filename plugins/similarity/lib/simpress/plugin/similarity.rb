2e52929350aefb247411d06fb05891e43e9c5efc
kinjouj <kinjouj@gmail.com>
2026-01-17 06:30:47 +0900

    update

--- plugins/similarity/lib/simpress/plugin/similarity.rb
+++ plugins/similarity/lib/simpress/plugin/similarity.rb
@@ -5,42 +5,47 @@ module Simpress
     class Similarity
       extend Simpress::Plugin
 
+      CATEGORY_WEIGHT = 10
+
       def self.run(posts, _, _)
-        docs = posts.map(&:extract_keywords)
+        docs    = posts.map(&:extract_keywords)
         vectors = posts.zip(docs).map do |post, keywords|
           freq = Hash.new(0)
           keywords.each {|k| freq[k] += 1 }
-          post.categories.each {|category| freq[category.name] += 10 }
+          post.categories.each {|category| freq[category.name] += CATEGORY_WEIGHT }
           freq
         end
 
-        norms = vectors.map { |v| Math.sqrt(v.values.sum { |x| x * x }) }
-        keyword_map = Hash.new {|h, k| h[k] = Set.new }
+        related_posts = Array.new(posts.size) { [] }
+        norms         = vectors.map { |v| Math.sqrt(v.values.sum { |x| x * x }) }
+        keyword_map   = Hash.new {|h, k| h[k] = [] }
         docs.each_with_index {|keywords, i| keywords.each {|k| keyword_map[k] << i } }
-
-        related_posts_list = Array.new(posts.size) { [] }
 
         vectors.each_with_index do |vec1, i|
           norm1 = norms[i]
           next if norm1.zero?
 
-          candidate_indices = docs[i].flat_map {|k| keyword_map[k].to_a }.to_set
-          candidate_indices.delete(i)
+          candidate_indices = docs[i].flat_map {|k| keyword_map[k] }.uniq.reject {|j| j == i }
           candidate_indices.each do |j|
-            vec2  = vectors[j]
             norm2 = norms[j]
             next if norm2.zero?
 
-            product = vec1.size < vec2.size ? vec1.sum {|k, v| v * vec2[k] } : vec2.sum {|k, v| v * vec1[k] }
+            vec2    = vectors[j]
+            product = if vec1.size < vec2.size
+                        vec1.sum {|k, v| v * (vec2[k] || 0) }
+                      else
+                        vec2.sum {|k, v| v * (vec1[k] || 0) }
+                      end
+
             cosine = product / (norm1 * norm2)
-            related_posts_list[i] << [cosine, { title: posts[j].title, permalink: posts[j].permalink }] if cosine > 0.0
+            related_posts[i] << [cosine, { title: posts[j].title, permalink: posts[j].permalink }] if cosine.positive?
           end
         end
 
-        related_posts_list.each_with_index do |related_posts, i|
+        related_posts.each_with_index do |sim_posts, i|
           base_post = posts[i]
-          sim_posts = related_posts.max_by(5) {|cosine, _| cosine }.map {|_, post| post }
-          result = { title: base_post.title, keywords: docs[i].uniq, similarity: sim_posts }.to_json
+          similarity = sim_posts.max_by(5) {|cosine, _| cosine }.map {|_, post| post }
+          result = { title: base_post.title, keywords: docs[i].uniq, similarity: similarity }.to_json
 
           Simpress::Writer.write("similarity/#{base_post.id}.json", result)
         end
