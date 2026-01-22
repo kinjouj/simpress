dbc4fbb30b9048df18078e7e7476973347b0e4b0
kinjouj <kinjouj@gmail.com>
2026-01-23 04:28:14 +0900

    Refactor write method to remove overwrite option
    Removed overwrite parameter from write method and adjusted file existence check accordingly.

--- lib/simpress/writer.rb
+++ lib/simpress/writer.rb
@@ -5,9 +5,9 @@ require "simpress/config"
 module Simpress
   module Writer
     class << self
-      def write(file, data, overwrite = false)
+      def write(file, data)
         filepath = File.join(output_dir, file)
-        raise "FILE EXISTS: #{filepath}" if !overwrite && File.exist?(filepath)
+        raise "FILE EXISTS: #{filepath}" if File.exist?(filepath)
 
         FileUtils.mkdir_p(File.dirname(filepath))
         File.write(filepath, data)
